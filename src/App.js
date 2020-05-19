dea0faac4814c1fc475f021be198d18b116a607c
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-05-20 00:01:14 +0100

    React router added with components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// npm i react-router-dom
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import Home from './Component/Home'
+// import About from './Component/About';
+import Navbar from './Component/Navbar'
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+const Navbar = () => {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path='/'>
+                        <Home />
+                    </Route>
+                    <Route path='/about'>
+                        <About />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
+export default Navbar;
